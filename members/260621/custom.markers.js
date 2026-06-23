7f9288e427cacf5e0ee24f2188c2ee4002cd092e
AvgZing <25853984+AvgZing@users.noreply.github.com>
2026-06-22 21:51:23 -0700

    Add 260621 Map, Re-Enable Gallery

---
+++ members/260621/custom.markers.js
@@ -0,0 +1,285 @@
+/*
+
+This is a JavaScript file you can edit to add custom markers to the map.
+uNmINeD does not overwrite this file during map generation.
+
+Steps:
+
+    1. Edit this file using Notepad or a code editor (do not use document editors like Microsoft Word)
+    2. Change the line "isEnabled: false," to "isEnabled: true," to display the markers
+    3. Change or remove the example markers
+    4. Add your own markers
+
+Marker format:
+
+    {
+        x: X coordinate of the marker (in Minecraft block units),
+        z: Z coordinate of the marker (in Minecraft block units),
+        image: marker image URL to display (in quotes),
+        imageScale: scale of the image (e.g. 1 = display full size, 0.5 = display half size),
+        imageAnchor: [0.5, 1] means the tip of the pin is at the center-bottom of the image (see OpenLayers documentation for more info),
+        text: marker text do display (in quotes),
+        textColor: text color in HTML/CSS format (in quotes),
+        offsetX: horizontal pixel offset of the text,
+        offsetY: vertical pixel offset of the text,
+        font: text font in HTML/CSS format (in quotes),
+    },
+
+Things to keep in mind:
+
+* There are opening and closing brackets for each marker "{" and "}"
+* Property names are case sensitive (i.e. "textColor" is okay, "TextColor" is not)
+* There is a comma (",") at the end of each line except the opening brackets ("{")
+
+You can use https://mapmarker.io/editor to generate custom pin images.
+Use the imageScale property if the pin image is too large.
+
+*/
+
+UnminedCustomMarkers = {
+  isEnabled: true,
+
+  markers: [
+    {
+      x: 0,
+      z: 0,
+      text: "SPAWN",
+      textColor: "red",
+      offsetX: 0,
+      offsetY: 0,
+      font: "bold 20px Calibri,sans serif",
+    },
+
+    {
+      x: -214,
+      z: -478,
+      text: "Jean",
+      textColor: "red",
+      offsetX: 0,
+      offsetY: 0,
+      font: "bold 12px Calibri,sans serif",
+    },
+
+    {
+      x: 333,
+      z: 1492,
+      text: "Artemis",
+      textColor: "red",
+      offsetX: 0,
+      offsetY: 0,
+      font: "bold 12px Calibri,sans serif",
+    },
+
+    {
+      x: 1343,
+      z: 910,
+      text: "RMD",
+      textColor: "red",
+      offsetX: 0,
+      offsetY: 0,
+      font: "bold 12px Calibri,sans serif",
+    },
+
+    {
+      x: -523,
+      z: -875,
+      text: "DJ",
+      textColor: "red",
+      offsetX: 0,
+      offsetY: 0,
+      font: "bold 12px Calibri,sans serif",
+    },
+
+    {
+      x: -1413,
+      z: 180,
+      text: "Phroggy",
+      textColor: "red",
+      offsetX: 0,
+      offsetY: 0,
+      font: "bold 12px Calibri,sans serif",
+    },
+
+    {
+      x: -1797,
+      z: 394,
+      text: "Phroggy",
+      textColor: "red",
+      offsetX: 0,
+      offsetY: 0,
+      font: "bold 12px Calibri,sans serif",
+    },
+
+    {
+      x: -697,
+      z: -953,
+      text: "Myranda",
+      textColor: "red",
+      offsetX: 0,
+      offsetY: 0,
+      font: "bold 12px Calibri,sans serif",
+    },
+
+    {
+      x: -326,
+      z: 1633,
+      text: "Mars",
+      textColor: "red",
+      offsetX: 0,
+      offsetY: 0,
+      font: "bold 12px Calibri,sans serif",
+    },
+
+    {
+      x: -500,
+      z: -500,
+      text: "Zing",
+      textColor: "red",
+      offsetX: 0,
+      offsetY: 0,
+      font: "bold 12px Calibri,sans serif",
+    },
+
+    {
+      x: -128,
+      z: 1135,
+      text: "Kiki",
+      textColor: "red",
+      offsetX: 0,
+      offsetY: 0,
+      font: "bold 12px Calibri,sans serif",
+    },
+
+    {
+      x: 963,
+      z: -3647,
+      text: "Kiki",
+      textColor: "red",
+      offsetX: 0,
+      offsetY: 0,
+      font: "bold 12px Calibri,sans serif",
+    },
+
+    {
+      x: 626,
+      z: -592,
+      text: "RainbowCat",
+      textColor: "red",
+      offsetX: 0,
+      offsetY: 0,
+      font: "bold 12px Calibri,sans serif",
+    },
+
+    {
+      x: 780,
+      z: 580,
+      text: "Raven",
+      textColor: "red",
+      offsetX: 0,
+      offsetY: 0,
+      font: "bold 12px Calibri,sans serif",
+    },
+
+    {
+      x: -46,
+      z: 1284,
+      text: "Mazer",
+      textColor: "red",
+      offsetX: 0,
+      offsetY: 0,
+      font: "bold 12px Calibri,sans serif",
+    },
+
+    {
+      x: -75,
+      z: 3674,
+      text: "Mazer",
+      textColor: "red",
+      offsetX: 0,
+      offsetY: 0,
+      font: "bold 12px Calibri,sans serif",
+    },
+
+    {
+      x: -352,
+      z: -1196,
+      text: "Ziukeal",
+      textColor: "red",
+      offsetX: 0,
+      offsetY: 0,
+      font: "bold 12px Calibri,sans serif",
+    },
+
+    {
+      x: -1326,
+      z: 753,
+      text: "Oakie",
+      textColor: "red",
+      offsetX: 0,
+      offsetY: 0,
+      font: "bold 12px Calibri,sans serif",
+    },
+
+    {
+      x: -772,
+      z: 1159,
+      text: "Zombie, Swine",
+      textColor: "red",
+      offsetX: 0,
+      offsetY: 0,
+      font: "bold 12px Calibri,sans serif",
+    },
+
+    {
+      x: -427,
+      z: 1162,
+      text: "Swine",
+      textColor: "red",
+      offsetX: 0,
+      offsetY: 0,
+      font: "bold 12px Calibri,sans serif",
+    },
+
+    {
+      x: 3841,
+      z: 149,
+      text: "Starling",
+      textColor: "red",
+      offsetX: 0,
+      offsetY: 0,
+      font: "bold 12px Calibri,sans serif",
+    },
+
+    {
+      x: 1978,
+      z: 2388,
+      text: "Starling",
+      textColor: "red",
+      offsetX: 0,
+      offsetY: 0,
+      font: "bold 12px Calibri,sans serif",
+    },
+
+    {
+      x: 435,
+      z: 1582,
+      text: "Loach",
+      textColor: "red",
+      offsetX: 0,
+      offsetY: 0,
+      font: "bold 12px Calibri,sans serif",
+    },
+
+    {
+      x: -191,
+      z: 1108,
+      text: "Frog",
+      textColor: "red",
+      offsetX: 0,
+      offsetY: 0,
+      font: "bold 12px Calibri,sans serif",
+    },
+
+    // do not delete the following two closing brackets
+  ],
+};
